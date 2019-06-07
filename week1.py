0b9d5c98d044721454d8bde9f081713c02e0c9f3
stevegbrooks <stevegbrooks@gmail.com>
2019-06-07 15:53:11 +0800

    updating gitignore

---
+++ week1.py
@@ -0,0 +1,6 @@
+#Week 1 assignment:
+#
+# 1. download the XML data from clinicaltrials.gov for the search term 'irritable bowel disease'
+# 2. bring it into python and try to build a prototype descriptive stats dashboard
+
+
